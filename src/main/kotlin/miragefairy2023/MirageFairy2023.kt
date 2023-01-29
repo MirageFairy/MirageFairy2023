64abac815d328d12fe61f7efa3f8b2c3e43698f5
MirageFairy <>
2023-01-29 10:34:33 +0900

    add: xarpite

--- src/main/kotlin/miragefairy2023/MirageFairy2023.kt
+++ src/main/kotlin/miragefairy2023/MirageFairy2023.kt
@@ -1,9 +1,18 @@
 package miragefairy2023
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.item.Item
+import net.minecraft.item.ItemGroup
+import net.minecraft.util.Identifier
+import net.minecraft.util.registry.Registry
 
 object MirageFairy2023 : ModInitializer {
-    override fun onInitialize() {
+    val modId = "miragefairy2023"
 
+    val XARPITE = Item(FabricItemSettings().group(ItemGroup.MATERIALS))
+
+    override fun onInitialize() {
+        Registry.register(Registry.ITEM, Identifier(modId, "xarpite"), XARPITE)
     }
 }
