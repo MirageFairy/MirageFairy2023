2117ae1c15f670fd73e98d11149451a2703a4bfd
MirageFairy <>
2023-03-21 23:44:27 +0900

    internal: add: blockScope

---
+++ src/main/kotlin/miragefairy2023/util/blockScope.kt
@@ -0,0 +1,22 @@
+@file:Suppress("MemberVisibilityCanBePrivate", "unused")
+
+package miragefairy2023.util
+
+import miragefairy2023.core.init.InitializationScope
+import net.minecraft.block.Block
+import net.minecraft.util.Identifier
+import net.minecraft.util.registry.Registry
+
+class BlockScope<T : Block>(val initializationScope: InitializationScope) {
+    lateinit var item: T
+}
+
+fun <T : Block> InitializationScope.block(blockId: String, blockCreator: () -> T, block: (BlockScope<T>.() -> Unit)? = null): () -> T {
+    val scope = BlockScope<T>(this)
+    blockRegistration {
+        scope.item = blockCreator()
+        Registry.register(Registry.BLOCK, Identifier(modId, blockId), scope.item)
+    }
+    if (block != null) block(scope)
+    return { scope.item }
+}
