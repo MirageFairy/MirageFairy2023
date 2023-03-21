901d40556afdadb997c5b42a5be9a7b9477aa9ea
MirageFairy <>
2023-03-21 19:49:54 +0900

    internal: add: util\player

---
+++ src/client/kotlin/miragefairy2023/util/player.kt
@@ -0,0 +1,7 @@
+@file:Suppress("MemberVisibilityCanBePrivate", "unused")
+
+package miragefairy2023.util
+
+import net.minecraft.client.MinecraftClient
+
+fun getClientPlayer() = MinecraftClient.getInstance().player
