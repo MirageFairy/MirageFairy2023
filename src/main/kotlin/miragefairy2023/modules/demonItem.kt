f229f2af16a00f8bceee4d1d991a1a0dabb40109
MirageFairy <>
2023-03-27 08:57:57 +0900

    ADD: ミラ粉を右クリックで妖精をランダム召還

--- src/main/kotlin/miragefairy2023/modules/demonItem.kt
+++ src/main/kotlin/miragefairy2023/modules/demonItem.kt
@@ -2,6 +2,7 @@ package miragefairy2023.modules
 
 import miragefairy2023.SlotContainer
 import miragefairy2023.module
+import miragefairy2023.util.createItemStack
 import miragefairy2023.util.enJa
 import miragefairy2023.util.enJaItem
 import miragefairy2023.util.gray
@@ -20,11 +21,16 @@ import net.minecraft.data.server.RecipeProvider
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder
 import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvents
 import net.minecraft.text.Text
+import net.minecraft.util.Hand
 import net.minecraft.util.Identifier
+import net.minecraft.util.TypedActionResult
 import net.minecraft.world.World
 
 
@@ -49,13 +55,13 @@ enum class DemonItemCard(
         "秩序の叛乱、天地創造の逆光。",
     ),
     TINY_MIRAGE_FLOUR(
-        { DemonItem(it) },
+        { MirageFlourItem(it, 1) },
         "tiny_mirage_flour", "Tiny Pile of Mirage Flour", "ミラージュの花粉",
         "Compose the body of Mirage fairy",
         "ささやかな温もりを、てのひらの上に。",
     ),
     MIRAGE_FLOUR(
-        { DemonItem(it) },
+        { MirageFlourItem(it, 8) },
         "mirage_flour", "Mirage Flour", "ミラージュフラワー",
         "Containing metallic organic matter",
         "創発のファンタズム",
@@ -170,9 +176,48 @@ val demonItemModule = module {
 }
 
 
-class DemonItem(settings: Settings) : Item(settings) {
+open class DemonItem(settings: Settings) : Item(settings) {
     override fun appendTooltip(stack: ItemStack, world: World?, tooltip: MutableList<Text>, context: TooltipContext) {
         super.appendTooltip(stack, world, tooltip, context)
         tooltip += text { translate("$translationKey.poem").gray }
+    }
+}
+
+class MirageFlourItem(settings: Settings, private val charge: Int) : DemonItem(settings) {
+    override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
+        val itemStack = user.getStackInHand(hand)
+
+        itemStack.decrement(1)
+
+        if (!world.isClient) {
+
+            repeat(charge) {
+
+                val r = world.random.nextDouble()
+                val fairyItemStack = when {
+                    r < 0.00003 -> FairyCard.TIME()
+                    r < 0.0001 -> FairyCard.SUN()
+                    r < 0.0003 -> FairyCard.WARDEN()
+                    r < 0.001 -> FairyCard.NIGHT()
+                    r < 0.003 -> FairyCard.PLAYER()
+                    r < 0.01 -> FairyCard.IRON()
+                    r < 0.03 -> FairyCard.FOREST()
+                    r < 0.1 -> FairyCard.SKELETON()
+                    r < 0.3 -> FairyCard.DIRT()
+                    else -> FairyCard.AIR()
+                }.createItemStack()
+                val itemEntity = user.dropItem(fairyItemStack, false)
+                if (itemEntity != null) {
+                    itemEntity.resetPickupDelay()
+                    itemEntity.owner = user.uuid
+                }
+
+            }
+
+            world.playSound(null, user.x, user.y, user.z, SoundEvents.BLOCK_DEEPSLATE_BREAK, SoundCategory.NEUTRAL, 1.0f, 1.0f)
+
+        }
+
+        return TypedActionResult.success(itemStack, world.isClient)
     }
 }
