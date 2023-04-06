a4396f91184b8f1edfcb27d942007490f692c7c1
MirageFairy <>
2023-04-07 07:39:04 +0900

    comment:

--- src/main/kotlin/miragefairy2023/modules/passiveskill/passiveSkill.kt
+++ src/main/kotlin/miragefairy2023/modules/passiveskill/passiveSkill.kt
@@ -50,12 +50,14 @@ interface PassiveSkillEffect {
     /**
      * 任意のタイミングで更新されうる持続的な効果を更新します。
      * プレイヤーやサーバーのリロードに伴って揮発するか、制限時間付きの効果を使用する必要があります。
+     * このメソッドは必ず論理サーバーで呼び出されます。
      * @param terminators プレイヤーのアンロードおよびサーバーの終了時は呼び出されません。
      */
     fun update(player: PlayerEntity, passiveSkillVariable: MutableMap<Identifier, Any>, initializers: MutableList<() -> Unit>, terminators: MutableList<() -> Unit>) = Unit
 
     /**
      * 10秒おきに呼び出されるアクションを発揮します。
+     * このメソッドは必ず論理サーバーで呼び出されます。
      */
     fun affect(player: PlayerEntity) = Unit
 }
