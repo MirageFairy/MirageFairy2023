3c18c6e6972d612cfdfae44c38826831610d647a
MirageFairy <>
2023-04-07 07:58:08 +0900

    VIEW: パッシブスキルの表記

--- src/main/kotlin/miragefairy2023/modules/passiveskill/passiveSkillEffects.kt
+++ src/main/kotlin/miragefairy2023/modules/passiveskill/passiveSkillEffects.kt
@@ -52,7 +52,7 @@ abstract class AttributePassiveSkillEffect : PassiveSkillEffect {
 }
 
 class MovementSpeedPassiveSkillEffect(override val power: Double) : AttributePassiveSkillEffect() {
-    override fun getText() = text { translate("attribute.name.generic.movement_speed") + " "() + (power * 100 formatAs "%+.0f%%")() }
+    override fun getText() = text { translate("attribute.name.generic.movement_speed") + ": "() + (power * 100 formatAs "%+.0f%%")() }
     override val uuid: UUID = UUID.fromString("378C9369-6CC3-4B45-AADD-5B221DF26ED0")
     override val identifier = Identifier(MirageFairy2023.modId, "movement_speed")
     override val entityAttribute: EntityAttribute get() = EntityAttributes.GENERIC_MOVEMENT_SPEED
@@ -60,7 +60,7 @@ class MovementSpeedPassiveSkillEffect(override val power: Double) : AttributePas
 }
 
 class AttackDamagePassiveSkillEffect(override val power: Double) : AttributePassiveSkillEffect() {
-    override fun getText() = text { translate("attribute.name.generic.attack_damage") + " "() + (power formatAs "%+.1f")() }
+    override fun getText() = text { translate("attribute.name.generic.attack_damage") + ": "() + (power formatAs "%+.1f")() }
     override val uuid: UUID = UUID.fromString("19306783-21EE-4A02-AC1F-46FFECE309A2")
     override val identifier = Identifier(MirageFairy2023.modId, "attack_damage")
     override val entityAttribute: EntityAttribute = EntityAttributes.GENERIC_ATTACK_DAMAGE
@@ -68,7 +68,7 @@ class AttackDamagePassiveSkillEffect(override val power: Double) : AttributePass
 }
 
 class MaxHealthPassiveSkillEffect(override val power: Double) : AttributePassiveSkillEffect() {
-    override fun getText() = text { translate("attribute.name.generic.max_health") + " "() + (power formatAs "%+.1f")() }
+    override fun getText() = text { translate("attribute.name.generic.max_health") + ": "() + (power formatAs "%+.1f")() }
     override val uuid: UUID = UUID.fromString("A3610FD7-694C-443C-B9D3-7F2815526EA7")
     override val identifier = Identifier(MirageFairy2023.modId, "max_health")
     override val entityAttribute: EntityAttribute = EntityAttributes.GENERIC_MAX_HEALTH
@@ -76,7 +76,7 @@ class MaxHealthPassiveSkillEffect(override val power: Double) : AttributePassive
 }
 
 class StatusEffectPassiveSkillEffect(private val statusEffect: StatusEffect, private val amplifier: Int, private val additionalSeconds: Int = 0) : PassiveSkillEffect {
-    override fun getText() = text { translate(statusEffect.translationKey) + " Lv.${amplifier + 1}"() }
+    override fun getText() = text { translate(statusEffect.translationKey) + ": Level ${amplifier + 1}"() }
     override fun affect(player: PlayerEntity) {
         player.addStatusEffect(StatusEffectInstance(statusEffect, 20 * (10 + 1 + additionalSeconds), amplifier, true, false, true))
     }
@@ -84,10 +84,10 @@ class StatusEffectPassiveSkillEffect(private val statusEffect: StatusEffect, pri
 
 class ExperiencePassiveSkillEffect(private val amount: Double) : PassiveSkillEffect {
     companion object {
-        val key = Translation("${MirageFairy2023.modId}.passive_skill.effect.experience", "Experience", "経験値")
+        val key = Translation("${MirageFairy2023.modId}.passive_skill.effect.experience", "Experience: %s", "経験値: %s")
     }
 
-    override fun getText() = text { key() + " +$amount"() }
+    override fun getText() = text { key(amount formatAs "%+.1f") }
     override fun affect(player: PlayerEntity) {
         val actualAmount = player.world.random.randomInt(amount)
         if (actualAmount > 0) player.addExperience(actualAmount)
