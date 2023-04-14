ac0ca0f1af982023d8a4b39807fa654dfcba5fd8
MirageFairy <>
2023-04-14 11:46:22 +0900

    internal: add: jsonArrayOf

--- src/main/kotlin/miragefairy2023/util/gson.kt
+++ src/main/kotlin/miragefairy2023/util/gson.kt
@@ -2,6 +2,7 @@
 
 package miragefairy2023.util
 
+import com.google.gson.JsonArray
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import com.google.gson.JsonPrimitive
@@ -10,6 +11,13 @@ fun jsonObjectOf(vararg pairs: Pair<String, JsonElement>) = jsonObjectOf(pairs.t
 fun jsonObjectOf(pairs: List<Pair<String, JsonElement>>) = JsonObject().also { json ->
     pairs.forEach {
         json.add(it.first, it.second)
+    }
+}
+
+fun jsonArrayOf(vararg items: JsonElement) = jsonArrayOf(items.toList())
+fun jsonArrayOf(items: List<JsonElement>) = JsonArray().also { json ->
+    items.forEach {
+        json.add(it)
     }
 }
 
