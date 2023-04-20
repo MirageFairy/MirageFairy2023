4a889e1985860e09bc3548b4d59f07ef88dd0890
MirageFairy <>
2023-04-21 07:43:46 +0900

    internal: util\identifier: add: concat

--- src/main/kotlin/miragefairy2023/util/identifier.kt
+++ src/main/kotlin/miragefairy2023/util/identifier.kt
@@ -5,3 +5,6 @@ package miragefairy2023.util
 import net.minecraft.util.Identifier
 
 fun String.toIdentifier() = Identifier(this)
+
+infix fun String.concat(identifier: Identifier) = Identifier(identifier.namespace, this + identifier.path)
+infix fun Identifier.concat(suffix: String) = Identifier(this.namespace, this.path + suffix)
