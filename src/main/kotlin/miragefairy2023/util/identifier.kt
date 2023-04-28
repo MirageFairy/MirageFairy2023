9763f3e821c7ff1c76162dbf160f95169c68ea93
MirageFairy <>
2023-04-29 06:48:00 +0900

    cleanup: remove: String.toIdentifier

--- src/main/kotlin/miragefairy2023/util/identifier.kt
+++ src/main/kotlin/miragefairy2023/util/identifier.kt
@@ -4,7 +4,5 @@ package miragefairy2023.util
 
 import net.minecraft.util.Identifier
 
-fun String.toIdentifier() = Identifier(this)
-
 infix fun String.concat(identifier: Identifier) = Identifier(identifier.namespace, this + identifier.path)
 infix fun Identifier.concat(suffix: String) = Identifier(this.namespace, this.path + suffix)
