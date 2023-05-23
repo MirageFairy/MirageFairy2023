d38a3d52abee9e23ab1226d315ccc34b4b50271f
MirageFairy <>
2023-05-23 14:47:56 +0900

    refactor: registerFairyHouse: extract

--- src/main/kotlin/miragefairy2023/modules/fairyhouse/FairyHouseModule.kt
+++ src/main/kotlin/miragefairy2023/modules/fairyhouse/FairyHouseModule.kt
@@ -1,27 +1,13 @@
 package miragefairy2023.modules.fairyhouse
 
-import miragefairy2023.MirageFairy2023
 import miragefairy2023.RenderingProxyBlockEntity
 import miragefairy2023.module
 import miragefairy2023.modules.DemonItemCard
-import miragefairy2023.modules.commonItemGroup
 import miragefairy2023.modules.invoke
-import miragefairy2023.util.gray
 import miragefairy2023.util.identifier
 import miragefairy2023.util.init.FeatureSlot
-import miragefairy2023.util.init.block
-import miragefairy2023.util.init.blockEntity
 import miragefairy2023.util.init.criterion
-import miragefairy2023.util.init.enJa
-import miragefairy2023.util.init.enJaBlock
-import miragefairy2023.util.init.generateDefaultBlockLootTable
-import miragefairy2023.util.init.generateHorizontalFacingBlockState
 import miragefairy2023.util.init.group
-import miragefairy2023.util.init.item
-import miragefairy2023.util.text
-import miragefairy2023.util.yellow
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings
-import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.fabricmc.fabric.api.tag.convention.v1.ConventionalItemTags
 import net.minecraft.block.AbstractBlock
 import net.minecraft.block.Block
@@ -29,17 +15,13 @@ import net.minecraft.block.BlockState
 import net.minecraft.block.Material
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.block.entity.BlockEntityType
-import net.minecraft.client.item.TooltipContext
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder
 import net.minecraft.item.BlockItem
-import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
 import net.minecraft.sound.BlockSoundGroup
 import net.minecraft.tag.BlockTags
 import net.minecraft.tag.TagKey
-import net.minecraft.text.Text
 import net.minecraft.util.math.BlockPos
-import net.minecraft.world.World
 
 class FairyHouseCard<B, BE>(
     val path: String,
@@ -72,38 +54,6 @@ class FairyHouseCard<B, BE>(
 
 object FairyHouseModule {
     val init = module {
-
-        fun <B, BE> registerFairyHouse(card: FairyHouseCard<B, BE>) where B : Block, BE : BlockEntity, BE : RenderingProxyBlockEntity {
-            card.block = block(card.path, { card.blockCreator(FabricBlockSettings.of(card.material).sounds(card.soundGroup).requiresTool().strength(2.0F).nonOpaque()) }) {
-
-                // レンダリング
-                generateHorizontalFacingBlockState()
-                onInitializeClient { MirageFairy2023.clientProxy!!.registerCutoutBlockRenderLayer(feature) }
-
-                // 翻訳
-                enJaBlock({ feature }, card.enName, card.jaName)
-                enJa({ "${feature.translationKey}.poem" }, card.enPoem, card.jaPoem)
-                enJa({ "${feature.translationKey}.description" }, card.enDescription, card.jaDescription)
-
-                // レシピ
-                onGenerateBlockTags { it(BlockTags.PICKAXE_MINEABLE).add(feature) }
-                if (card.needsToolTag != null) onGenerateBlockTags { it(card.needsToolTag).add(feature) }
-                generateDefaultBlockLootTable()
-
-            }
-            card.blockEntityType = blockEntity(card.path, card.blockEntityCreator, { card.block.feature }) {
-                onInitializeClient { MirageFairy2023.clientProxy!!.registerRenderingProxyBlockEntityRendererFactory(feature) }
-            }
-            card.blockItem = item(card.path, {
-                object : BlockItem(card.block.feature, FabricItemSettings().group(commonItemGroup)) {
-                    override fun appendTooltip(stack: ItemStack, world: World?, tooltip: MutableList<Text>, context: TooltipContext) {
-                        super.appendTooltip(stack, world, tooltip, context)
-                        tooltip += text { translate("$translationKey.poem").gray }
-                        tooltip += text { translate("$translationKey.description").yellow }
-                    }
-                }
-            })
-        }
 
         // 妖精の水汲み所
         registerFairyHouse(FairyHouseCard.FAIRY_FLUID_DRAINER)
--- src/main/kotlin/miragefairy2023/modules/fairyhouse/util.kt
+++ src/main/kotlin/miragefairy2023/modules/fairyhouse/util.kt
@@ -1,10 +1,65 @@
 package miragefairy2023.modules.fairyhouse
 
+import miragefairy2023.InitializationScope
+import miragefairy2023.MirageFairy2023
 import miragefairy2023.RenderingProxy
+import miragefairy2023.RenderingProxyBlockEntity
 import miragefairy2023.modules.DemonParticleTypeCard
+import miragefairy2023.modules.commonItemGroup
+import miragefairy2023.util.gray
+import miragefairy2023.util.init.block
+import miragefairy2023.util.init.blockEntity
+import miragefairy2023.util.init.enJa
+import miragefairy2023.util.init.enJaBlock
+import miragefairy2023.util.init.generateDefaultBlockLootTable
+import miragefairy2023.util.init.generateHorizontalFacingBlockState
+import miragefairy2023.util.init.item
+import miragefairy2023.util.text
+import miragefairy2023.util.yellow
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
+import net.minecraft.block.Block
+import net.minecraft.block.entity.BlockEntity
+import net.minecraft.client.item.TooltipContext
+import net.minecraft.item.BlockItem
 import net.minecraft.item.ItemStack
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.tag.BlockTags
+import net.minecraft.text.Text
 import net.minecraft.util.math.Vec3d
+import net.minecraft.world.World
+
+fun <B, BE> InitializationScope.registerFairyHouse(card: FairyHouseCard<B, BE>) where B : Block, BE : BlockEntity, BE : RenderingProxyBlockEntity {
+    card.block = block(card.path, { card.blockCreator(FabricBlockSettings.of(card.material).sounds(card.soundGroup).requiresTool().strength(2.0F).nonOpaque()) }) {
+
+        // レンダリング
+        generateHorizontalFacingBlockState()
+        onInitializeClient { MirageFairy2023.clientProxy!!.registerCutoutBlockRenderLayer(feature) }
+
+        // 翻訳
+        enJaBlock({ feature }, card.enName, card.jaName)
+        enJa({ "${feature.translationKey}.poem" }, card.enPoem, card.jaPoem)
+        enJa({ "${feature.translationKey}.description" }, card.enDescription, card.jaDescription)
+
+        // レシピ
+        onGenerateBlockTags { it(BlockTags.PICKAXE_MINEABLE).add(feature) }
+        if (card.needsToolTag != null) onGenerateBlockTags { it(card.needsToolTag).add(feature) }
+        generateDefaultBlockLootTable()
+
+    }
+    card.blockEntityType = blockEntity(card.path, card.blockEntityCreator, { card.block.feature }) {
+        onInitializeClient { MirageFairy2023.clientProxy!!.registerRenderingProxyBlockEntityRendererFactory(feature) }
+    }
+    card.blockItem = item(card.path, {
+        object : BlockItem(card.block.feature, FabricItemSettings().group(commonItemGroup)) {
+            override fun appendTooltip(stack: ItemStack, world: World?, tooltip: MutableList<Text>, context: TooltipContext) {
+                super.appendTooltip(stack, world, tooltip, context)
+                tooltip += text { translate("$translationKey.poem").gray }
+                tooltip += text { translate("$translationKey.description").yellow }
+            }
+        }
+    })
+}
 
 fun ServerWorld.spawnCraftingCompletionParticles(pos: Vec3d) {
     this.spawnParticles(DemonParticleTypeCard.DESCENDING_MAGIC.particleType, pos.x, pos.y, pos.z, 5, 0.0, 0.0, 0.0, 0.02)
