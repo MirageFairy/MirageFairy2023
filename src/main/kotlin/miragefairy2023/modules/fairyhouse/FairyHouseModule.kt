da1dea93084fd66fbe5b36faa2165a4333e5bce0
MirageFairy <>
2023-05-23 14:55:08 +0900

    refactor: extract: fairyFluidDrainerModule

--- src/main/kotlin/miragefairy2023/modules/fairyhouse/FairyHouseModule.kt
+++ src/main/kotlin/miragefairy2023/modules/fairyhouse/FairyHouseModule.kt
@@ -1,44 +1,9 @@
 package miragefairy2023.modules.fairyhouse
 
 import miragefairy2023.module
-import miragefairy2023.modules.DemonItemCard
-import miragefairy2023.modules.invoke
-import miragefairy2023.util.identifier
-import miragefairy2023.util.init.criterion
-import miragefairy2023.util.init.group
-import net.fabricmc.fabric.api.tag.convention.v1.ConventionalItemTags
-import net.minecraft.block.Material
-import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder
-import net.minecraft.item.Items
-import net.minecraft.sound.BlockSoundGroup
-import net.minecraft.tag.BlockTags
-
-val fairyFluidDrainer = FairyHouseCard(
-    "fairy_fluid_drainer", ::FairyFluidDrainerBlock, ::FairyFluidDrainerBlockEntity,
-    "Fairy Fluid Drainer", "妖精の水汲み所",
-    "Causes anti-Brownian motion", "覆水、盆に返る。",
-    "Place a liquid fairy and a bucket", "液体系妖精と空バケツを配置",
-    Material.METAL, BlockSoundGroup.METAL, BlockTags.NEEDS_STONE_TOOL,
-)
 
 object FairyHouseModule {
     val init = module {
-
-        // 妖精の水汲み所
-        registerFairyHouse(fairyFluidDrainer)
-        onGenerateRecipes {
-            ShapedRecipeJsonBuilder
-                .create(fairyFluidDrainer.blockItem.feature)
-                .pattern("FMB")
-                .pattern("III")
-                .input('I', ConventionalItemTags.IRON_INGOTS)
-                .input('F', Items.IRON_BARS)
-                .input('M', DemonItemCard.MIRANAGITE())
-                .input('B', Items.BUCKET)
-                .criterion(DemonItemCard.MIRANAGITE())
-                .group(fairyFluidDrainer.blockItem.feature)
-                .offerTo(it, fairyFluidDrainer.blockItem.feature.identifier)
-        }
-
+        fairyFluidDrainerModule()
     }
 }
--- src/main/kotlin/miragefairy2023/modules/fairyhouse/fairyFluidDrainer.kt
+++ src/main/kotlin/miragefairy2023/modules/fairyhouse/fairyFluidDrainer.kt
@@ -2,26 +2,37 @@ package miragefairy2023.modules.fairyhouse
 
 import miragefairy2023.RenderingProxy
 import miragefairy2023.api.FairyItem
+import miragefairy2023.module
+import miragefairy2023.modules.DemonItemCard
 import miragefairy2023.modules.DemonParticleTypeCard
 import miragefairy2023.modules.fairy.isLiquidFairy
+import miragefairy2023.modules.invoke
 import miragefairy2023.util.Inventory
 import miragefairy2023.util.castOr
 import miragefairy2023.util.createItemStack
 import miragefairy2023.util.get
+import miragefairy2023.util.identifier
+import miragefairy2023.util.init.criterion
+import miragefairy2023.util.init.group
 import miragefairy2023.util.notEmptyOrNull
 import miragefairy2023.util.set
 import mirrg.kotlin.hydrogen.or
+import net.fabricmc.fabric.api.tag.convention.v1.ConventionalItemTags
 import net.minecraft.block.BlockState
 import net.minecraft.block.Blocks
 import net.minecraft.block.FluidDrainable
 import net.minecraft.block.LeveledCauldronBlock
+import net.minecraft.block.Material
 import net.minecraft.block.ShapeContext
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.sound.BlockSoundGroup
 import net.minecraft.sound.SoundCategory
 import net.minecraft.sound.SoundEvent
 import net.minecraft.sound.SoundEvents
+import net.minecraft.tag.BlockTags
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 import net.minecraft.util.math.Vec3d
@@ -29,6 +40,34 @@ import net.minecraft.util.math.random.Random
 import net.minecraft.world.BlockView
 import net.minecraft.world.World
 import kotlin.jvm.optionals.getOrNull
+
+val fairyFluidDrainer = FairyHouseCard(
+    "fairy_fluid_drainer", ::FairyFluidDrainerBlock, ::FairyFluidDrainerBlockEntity,
+    "Fairy Fluid Drainer", "妖精の水汲み所",
+    "Causes anti-Brownian motion", "覆水、盆に返る。",
+    "Place a liquid fairy and a bucket", "液体系妖精と空バケツを配置",
+    Material.METAL, BlockSoundGroup.METAL, BlockTags.NEEDS_STONE_TOOL,
+)
+
+val fairyFluidDrainerModule = module {
+
+    // 妖精の水汲み所
+    registerFairyHouse(fairyFluidDrainer)
+    onGenerateRecipes {
+        ShapedRecipeJsonBuilder
+            .create(fairyFluidDrainer.blockItem.feature)
+            .pattern("FMB")
+            .pattern("III")
+            .input('I', ConventionalItemTags.IRON_INGOTS)
+            .input('F', Items.IRON_BARS)
+            .input('M', DemonItemCard.MIRANAGITE())
+            .input('B', Items.BUCKET)
+            .criterion(DemonItemCard.MIRANAGITE())
+            .group(fairyFluidDrainer.blockItem.feature)
+            .offerTo(it, fairyFluidDrainer.blockItem.feature.identifier)
+    }
+
+}
 
 interface FairyFluidDrainerRecipe {
     companion object {
