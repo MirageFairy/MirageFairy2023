2e4612a815af0838638801a1a5b3883c0ad7a0aa
MirageFairy <>
2023-06-04 04:35:44 +0900

    ADD: fairy slot

--- src/main/kotlin/miragefairy2023/modules/toolItem.kt
+++ src/main/kotlin/miragefairy2023/modules/toolItem.kt
@@ -2,6 +2,7 @@ package miragefairy2023.modules
 
 import miragefairy2023.InitializationScope
 import miragefairy2023.MirageFairy2023
+import miragefairy2023.TrinketsSlotDataProvider
 import miragefairy2023.api.PassiveSkill
 import miragefairy2023.api.PassiveSkillItem
 import miragefairy2023.module
@@ -72,6 +73,7 @@ enum class ToolItemCard(
 }
 
 enum class TrinketsSlot(val groupName: String, val slotName: String) {
+    HEAD_FAIRY("head", "fairy"),
     CHEST_NECKLACE("chest", "necklace"),
     ;
 
@@ -82,7 +84,15 @@ enum class TrinketsSlot(val groupName: String, val slotName: String) {
 val toolItemModule = module {
 
     // Trinkets
+    onGenerateTrinketsSlot {
+        it.slots += TrinketsSlot.HEAD_FAIRY.path to TrinketsSlotDataProvider.TrinketsSlotEntry(
+            Identifier("trinkets", "gui/slots/${TrinketsSlot.HEAD_FAIRY.slotName}"),
+            quickMovePredicates = listOf("trinkets:none"),
+        )
+    }
+    enJa("trinkets.slot.head.fairy", "Fairy", "妖精")
     onGenerateTrinketsEntities {
+        it.slots += TrinketsSlot.HEAD_FAIRY.path
         it.slots += TrinketsSlot.CHEST_NECKLACE.path
     }
 
