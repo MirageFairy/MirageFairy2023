3776a3373f9ff893776d2748d380c70981ef33fe
MirageFairy <>
2023-06-15 06:02:15 +0900

    internal: wave: resizing

--- src/main/kotlin/miragefairy2023/wave/main.kt
+++ src/main/kotlin/miragefairy2023/wave/main.kt
@@ -15,6 +15,8 @@ fun main() {
         "png" -> {
             inputFile
                 .readImage()
+                .resize((48000 * 0.348852 + 255).toInt(), (1 shl 8) / 2 + 1)
+                //.also { it.writeTo(inputFile.resolveSibling("1.png")) }
                 .generatePhase()
                 .fromSpectrogram(8, 0.00025)
                 .toWav()
