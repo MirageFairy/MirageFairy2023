8d87b7cad5f33c4b09f27fe833a4bdebac9fbf16
MirageFairy <>
2023-07-12 04:35:11 +0900

    format: BlockLootTable

--- src/main/kotlin/miragefairy2023/util/datagen/BlockLootTable.kt
+++ src/main/kotlin/miragefairy2023/util/datagen/BlockLootTable.kt
@@ -7,17 +7,13 @@ import net.minecraft.block.Block
 import net.minecraft.data.server.BlockLootTableGenerator
 import net.minecraft.loot.LootTable
 
-fun InitializationScope.generateBlockLootTable(block: Block, initializer: () -> LootTable.Builder) {
-    onGenerateBlockLootTables {
-        addDrop(block, initializer())
-    }
+fun InitializationScope.generateBlockLootTable(block: Block, initializer: () -> LootTable.Builder) = onGenerateBlockLootTables {
+    addDrop(block, initializer())
 }
 
 @Deprecated("Removing") // TODO remove
-fun InitializationScope.generateBlockLootTable(blockGetter: () -> Block, initializer: () -> LootTable.Builder) {
-    onGenerateBlockLootTables {
-        addDrop(blockGetter(), initializer())
-    }
+fun InitializationScope.generateBlockLootTable(blockGetter: () -> Block, initializer: () -> LootTable.Builder) = onGenerateBlockLootTables {
+    addDrop(blockGetter(), initializer())
 }
 
 fun InitializationScope.generateDefaultBlockLootTable(block: Block) = generateBlockLootTable(block) { BlockLootTableGenerator.drops(block) }
