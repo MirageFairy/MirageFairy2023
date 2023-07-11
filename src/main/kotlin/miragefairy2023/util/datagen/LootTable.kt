5bd2fc484973494556ea8c762b91ef283e12400c
MirageFairy <>
2023-07-12 03:54:27 +0900

    refactor: LootPoolEntry

--- src/main/kotlin/miragefairy2023/util/datagen/LootTable.kt
+++ src/main/kotlin/miragefairy2023/util/datagen/LootTable.kt
@@ -7,7 +7,12 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider
 import net.minecraft.item.ItemConvertible
 import net.minecraft.loot.LootPool
 import net.minecraft.loot.LootTable
+import net.minecraft.loot.entry.AlternativeEntry
+import net.minecraft.loot.entry.GroupEntry
+import net.minecraft.loot.entry.ItemEntry
+import net.minecraft.loot.entry.LeafEntry
 import net.minecraft.loot.entry.LootPoolEntry
+import net.minecraft.loot.entry.SequenceEntry
 import net.minecraft.loot.function.LootFunctionConsumingBuilder
 
 @Suppress("FunctionName")
@@ -24,6 +29,27 @@ fun LootPool(vararg entries: LootPoolEntry.Builder<*>, initializer: LootPool.Bui
     entries.forEach {
         this.with(it)
     }
+    initializer.invoke(this)
+}
+
+
+@Suppress("FunctionName")
+fun ItemLootPoolEntry(item: ItemConvertible, initializer: LeafEntry.Builder<*>.() -> Unit = {}): LeafEntry.Builder<*> = configure(ItemEntry.builder(item)!!) {
+    initializer.invoke(this)
+}
+
+@Suppress("FunctionName")
+fun AlternativeLootPoolEntry(vararg children: LootPoolEntry.Builder<*>, initializer: AlternativeEntry.Builder.() -> Unit = {}): AlternativeEntry.Builder = configure(AlternativeEntry.builder(*children)!!) {
+    initializer.invoke(this)
+}
+
+@Suppress("FunctionName")
+fun GroupLootPoolEntry(vararg children: LootPoolEntry.Builder<*>, initializer: GroupEntry.Builder.() -> Unit = {}): GroupEntry.Builder = configure(GroupEntry.create(*children)!!) {
+    initializer.invoke(this)
+}
+
+@Suppress("FunctionName")
+fun SequenceLootPoolEntry(vararg children: LootPoolEntry.Builder<*>, initializer: SequenceEntry.Builder.() -> Unit = {}): SequenceEntry.Builder = configure(SequenceEntry.create(*children)!!) {
     initializer.invoke(this)
 }
 
