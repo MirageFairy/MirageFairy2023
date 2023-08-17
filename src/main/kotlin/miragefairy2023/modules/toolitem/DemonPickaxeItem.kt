48e266cb98d46ae1a03918e41172bcc60487a5b6
MirageFairy <>
2023-08-18 05:14:16 +0900

    internal: PickaxeType: add: mineAll

--- src/main/kotlin/miragefairy2023/modules/toolitem/DemonPickaxeItem.kt
+++ src/main/kotlin/miragefairy2023/modules/toolitem/DemonPickaxeItem.kt
@@ -1,20 +1,26 @@
 package miragefairy2023.modules.toolitem
 
+import miragefairy2023.util.blockVisitor
+import miragefairy2023.util.breakBlock
+import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBlockTags
 import net.fabricmc.yarn.constants.MiningLevels
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
 import net.minecraft.enchantment.EnchantmentHelper
 import net.minecraft.enchantment.Enchantments
+import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.item.PickaxeItem
 import net.minecraft.item.ToolMaterial
+import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.sound.SoundCategory
 import net.minecraft.sound.SoundEvents
 import net.minecraft.tag.BlockTags
 import net.minecraft.tag.TagKey
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
+import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 
 class DemonPickaxeItem(
@@ -23,6 +29,7 @@ class DemonPickaxeItem(
     attackSpeed: Float,
     private val effectiveBlockTags: List<TagKey<Block>>,
     private val silkTouch: Boolean,
+    private val mineAll: Boolean,
     settings: Settings,
 ) : PickaxeItem(toolMaterial, attackDamage, attackSpeed, settings) {
     override fun getMiningSpeedMultiplier(stack: ItemStack, state: BlockState) = if (effectiveBlockTags.any { state.isIn(it) }) miningSpeed else 1.0F
@@ -35,6 +42,28 @@ class DemonPickaxeItem(
             itemMiningLevel < MiningLevels.STONE && state.isIn(BlockTags.NEEDS_STONE_TOOL) -> false
             else -> effectiveBlockTags.any { state.isIn(it) }
         }
+    }
+
+    override fun postMine(stack: ItemStack, world: World, state: BlockState, pos: BlockPos, miner: LivingEntity): Boolean {
+        super.postMine(stack, world, state, pos, miner)
+        if (mineAll) run fail@{
+            if (world.isClient) return@fail
+
+            if (miner !is ServerPlayerEntity) return@fail // 使用者がプレイヤーでない
+            if (!isSuitableFor(state)) return@fail // 掘ったブロックに対して特効でない
+            if (!state.isIn(ConventionalBlockTags.ORES)) return@fail // 掘ったブロックが鉱石ではない
+
+            // 発動
+
+            blockVisitor(pos, visitOrigins = false, maxDistance = 19, maxCount = 19) { _, _, toBlockPos ->
+                world.getBlockState(toBlockPos).block === state.block
+            }.forEach { (_, blockPos) ->
+                if (stack.isEmpty) return@fail // ツールの耐久値が枯渇した
+                if (stack.maxDamage - stack.damage <= 1) return@fail // ツールの耐久値が残り1
+                breakBlock(stack, world, blockPos, miner)
+            }
+        }
+        return true
     }
 
     override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
