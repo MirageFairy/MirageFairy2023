3a2555de3022db0401091af5455dd2f6227850de
MirageFairy <>
2023-08-18 09:13:28 +0900

    internal: add: breakBlockByMagic

--- src/main/kotlin/miragefairy2023/util/world.kt
+++ src/main/kotlin/miragefairy2023/util/world.kt
@@ -2,6 +2,7 @@ package miragefairy2023.util
 
 import net.minecraft.block.Block
 import net.minecraft.block.OperatorBlock
+import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.network.ServerPlayerInteractionManager
@@ -78,5 +79,34 @@ fun breakBlock(itemStack: ItemStack, world: World, blockPos: BlockPos, player: S
     val canHarvest = player.canHarvest(blockState)
     itemStack.postMine(world, blockState, blockPos, player)
     if (success && canHarvest) block.afterBreak(world, player, blockPos, blockState, blockEntity, newItemStack)
+    return true
+}
+
+/**
+ * 魔法効果としてブロックを壊します。
+ *
+ * [breakBlock]とは以下の点で異なります。
+ * - 近接武器の採掘不能特性を無視します。
+ * - 専用のツールが必要なブロックを、ツールの種類にかかわらず回収可能です。
+ * - [Item.postMine]を起動せず、アイテムの耐久値の減少などが発生しません。
+ */
+fun breakBlockByMagic(itemStack: ItemStack, world: World, blockPos: BlockPos, player: ServerPlayerEntity): Boolean {
+    val blockState = world.getBlockState(blockPos)
+    val blockEntity = world.getBlockEntity(blockPos)
+    val block = blockState.block
+
+    if (blockState.getHardness(world, blockPos) == -1F) return false // このブロックは破壊不能
+    if (block is OperatorBlock && !player.isCreativeLevelTwoOp) {
+        world.updateListeners(blockPos, blockState, blockState, Block.NOTIFY_ALL)
+        return false // コマンドブロックを破壊しようとした
+    }
+    if (player.isBlockBreakingRestricted(world, blockPos, player.interactionManager.gameMode)) return false // 破壊する権限がない
+
+    block.onBreak(world, blockPos, blockState, player)
+    val success = world.removeBlock(blockPos, false)
+    if (success) block.onBroken(world, blockPos, blockState)
+    if (player.isCreative) return true // クリエイティブの場合、ドロップを省略
+    val newItemStack = itemStack.copy()
+    if (success) block.afterBreak(world, player, blockPos, blockState, blockEntity, newItemStack)
     return true
 }
