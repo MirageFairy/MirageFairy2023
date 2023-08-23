4476d8e91d76ec1ce3b7be519c12ef49af4057bf
MirageFairy <>
2023-08-24 02:24:47 +0900

    ADD: biome: 地形生成

--- src/main/kotlin/miragefairy2023/modules/biome.kt
+++ src/main/kotlin/miragefairy2023/modules/biome.kt
@@ -1,17 +1,27 @@
 package miragefairy2023.modules
 
+import com.mojang.datafixers.util.Pair
 import miragefairy2023.MirageFairy2023
 import miragefairy2023.module
 import miragefairy2023.util.init.enJa
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.SpawnGroup
 import net.minecraft.util.Identifier
+import net.minecraft.util.registry.BuiltinRegistries
+import net.minecraft.util.registry.Registry
+import net.minecraft.util.registry.RegistryKey
 import net.minecraft.world.biome.Biome
 import net.minecraft.world.biome.BiomeEffects
 import net.minecraft.world.biome.GenerationSettings
 import net.minecraft.world.biome.SpawnSettings
 import net.minecraft.world.biome.SpawnSettings.SpawnEntry
+import net.minecraft.world.biome.source.util.MultiNoiseUtil
 import net.minecraft.world.gen.feature.DefaultBiomeFeatures
+import terrablender.api.ParameterUtils
+import terrablender.api.Region
+import terrablender.api.RegionType
+import terrablender.api.Regions
+import java.util.function.Consumer
 
 enum class BiomeCard(
     val path: String,
@@ -71,6 +81,23 @@ val biomeModule = module {
     BiomeCard.values().forEach { card ->
         onGenerateBiome {
             it.map[card.identifier] = card.biome
+        }
+        onTerraBlenderInitialized {
+            Regions.register(object : Region(Identifier(MirageFairy2023.modId, "fairy_forest"), RegionType.OVERWORLD, 1) {
+                override fun addBiomes(registry: Registry<Biome>, mapper: Consumer<Pair<MultiNoiseUtil.NoiseHypercube, RegistryKey<Biome>>>) {
+                    addBiome(
+                        mapper,
+                        ParameterUtils.Temperature.span(ParameterUtils.Temperature.ICY, ParameterUtils.Temperature.NEUTRAL),
+                        ParameterUtils.Humidity.span(ParameterUtils.Humidity.NEUTRAL, ParameterUtils.Humidity.HUMID),
+                        ParameterUtils.Continentalness.span(ParameterUtils.Continentalness.NEAR_INLAND, ParameterUtils.Continentalness.FAR_INLAND),
+                        ParameterUtils.Erosion.span(ParameterUtils.Erosion.EROSION_2, ParameterUtils.Erosion.EROSION_5),
+                        ParameterUtils.Weirdness.span(ParameterUtils.Weirdness.MID_SLICE_VARIANT_ASCENDING, ParameterUtils.Weirdness.MID_SLICE_VARIANT_DESCENDING),
+                        ParameterUtils.Depth.span(ParameterUtils.Depth.SURFACE, ParameterUtils.Depth.SURFACE),
+                        0.0F,
+                        RegistryKey.of(BuiltinRegistries.BIOME.key, BiomeCard.FAIRY_FOREST.identifier),
+                    )
+                }
+            })
         }
         enJa("biome.${card.identifier.toTranslationKey()}", card.en, card.ja)
     }
